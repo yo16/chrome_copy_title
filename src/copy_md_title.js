b17fbbcbf97ae783942d1e9d1211a3178a92dbf6
yo16 <xxyo1xx@gmail.com>
2022-12-17 12:54:02 +0900

    処理を追加

--- src/copy_md_title.js
+++ src/copy_md_title.js
@@ -1,6 +1,7 @@
 
 chrome.runtime.onInstalled.addListener((details) => {
     console.log(details.reason);
+    // コンテキストメニューに追加
     let menu_id = chrome.contextMenus.create({
         id: "copy_url_md_format",
         title: "Copy URL in MD format",
@@ -14,4 +15,11 @@ chrome.runtime.onInstalled.addListener((details) => {
         }*/
     });
     console.log('created menu id:'+menu_id);
+
+    // 選択時の処理
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+        console.log('context cliked!');
+        console.log(info);
+        console.log(tab);
+    });
 });
